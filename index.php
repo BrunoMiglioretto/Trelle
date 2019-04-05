9dacaa11ca3d6fe3876ee022015df48660cb4887
BrunoMiglioretto <Brunomiglioretto@hotmail.com>
2019-04-05 19:10:25 -0300

    Estrutura básica HTML e CSS

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Trelle</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="topo">
+            <h1>Trelle</h1>
+        </div>
+        <div class="quadros">
+            <div class="quadro">
+                <h2>A Fazer</h2>
+                <div class="cartoes">
+                    <div class="cartao">
+                        <p>Tarefa do Robson Git e GitHub</p>
+                    </div>
+                    <div class="cartao">
+                        <p>Terefa  </p>
+                    </div>
+                    <div class="cartao">
+                        <p>Terefa 2</p>
+                    </div>
+                    <div class="cartao">
+                        <p>Terefa 3 </p>
+                    </div>
+                    <div class="cartao">
+                        <p>Terefa 4 </p>
+                    </div>
+                    <div class="cartao">
+                        <p>Terefa 5 </p>
+                    </div>
+                    <div class="cartao">
+                        <p>Terefa 6 </p>
+                    </div>
+                    <div class="cartao">
+                        <p>Terefa 7 </p>
+                    </div>
+                    <div class="cartao">
+                        <p>Terefa 7 </p>
+                    </div>
+                    <div class="cartao">
+                        <p>Terefa 7 </p>
+                    </div>
+                </div>
+            </div>
+            <div class="quadro">
+                <h2>Fazendo</h2>
+                <div class="cartoes">
+                
+                </div>
+            </div>
+            <div class="quadro">
+                <h2>Feito</h2>
+                <div class="cartoes">
+                
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
