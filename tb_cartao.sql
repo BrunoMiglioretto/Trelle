bb4c0969ed02197978c9c392d48c6897d3ccdb22
BrunoMiglioretto <Brunomiglioretto@hotmail.com>
2019-04-11 01:08:58 -0300

    Add banco de dados

---
+++ tb_cartao.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 11-Abr-2019 às 06:08
+-- Versão do servidor: 10.1.35-MariaDB
+-- versão do PHP: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `trelle`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_cartao`
+--
+
+CREATE TABLE `tb_cartao` (
+  `id_cartao` int(11) NOT NULL,
+  `mensagem` varchar(100) DEFAULT NULL,
+  `data_entrega` date DEFAULT NULL,
+  `quadro` int(11) DEFAULT NULL,
+  `responsavel` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_cartao`
+--
+ALTER TABLE `tb_cartao`
+  ADD PRIMARY KEY (`id_cartao`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_cartao`
+--
+ALTER TABLE `tb_cartao`
+  MODIFY `id_cartao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
