d49b1fb44de9f26aceb91df2f887ece06a920d22
BrunoMiglioretto <Brunomiglioretto@hotmail.com>
2019-04-13 16:53:21 -0300

    Colocado função de dias restantes

--- php/atualizar/quadro1.php
+++ php/atualizar/quadro1.php
@@ -9,14 +9,17 @@ $cartao->execute();
 foreach($cartao as $carrega){
     $databd = $carrega["data_entrega"];
     $data = strtotime($databd);
+    $hoje = date("Y-m-d");
 
-    $dataF = date('d/m/Y',$data);
+    $dataH = date("Y-m-d", $data);
+    $diferenca = (strtotime($dataH) - strtotime($hoje)) /86400;
 
+    $dataF = date('d/m/Y', $data);
     echo "
         <div class='cartao' name='".$carrega["id_cartao"]."' aria-label='Responsavel: ".$carrega["responsavel"]."' data-microtip-position='bottom' role='tooltip'>
             <p>".$carrega["mensagem"]."</p>
             <p class='data'>".$dataF."<p>
-            <p class='dias'>4 Dias</p>
+            <p class='dias'>".$diferenca." Dia(s)</p>
         </div>
     ";
 }
